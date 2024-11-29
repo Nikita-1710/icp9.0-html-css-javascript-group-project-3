2424f2d4c9376151b9b7fc499f8a21431eda808f
Samradnya Thakare <samradnyathakare@gmail.com>
2024-11-30 01:01:50 +0530

    css is updated

--- js/about.js
+++ js/about.js
@@ -22,3 +22,15 @@ function back() {
     slide.src = images[index]
 }
 setInterval(next, 2000);
+
+// script.js
+document.getElementById('search-bar').addEventListener('input', function () {
+    const filter = this.value.toLowerCase();
+    const participants = document.querySelectorAll('.participant');
+  
+    participants.forEach((participant) => {
+      const name = participant.textContent.toLowerCase();
+      participant.style.display = name.includes(filter) ? 'block' : 'none';
+    });
+  });
+  
