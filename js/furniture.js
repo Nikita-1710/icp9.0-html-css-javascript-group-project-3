20c8ffa7ca12a38a755aca8ad1df488840786c05
aman85662 <118710449+aman85662@users.noreply.github.com>
2024-11-30 17:47:25 +0530

    product-card added

--- js/furniture.js
+++ js/furniture.js
@@ -4,5 +4,31 @@ function opensidebar(){
 }
 function closesidebar(){
     const sidebarElement = document.getElementById('sidebar');
-    sidebarElement.style.visibility = 'hidden';
+    sidebarElement.style.visibility= 'hidden';
 }
+const images = [
+];
+
+let currentIndex = 0;
+
+function showImage(index) {
+    const slideshowImage = document.getElementById('slideshow-image');
+    slideshowImage.src = images[index];
+}
+function prevImage() {
+    if (currentIndex === 0) {
+        currentIndex = images.length - 1;
+    } else {
+        currentIndex--;
+    }
+    showImage(currentIndex);
+}
+function nextImage() {
+    if (currentIndex === images.length - 1) {
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+    showImage(currentIndex);
+}
+showImage(currentIndex);
