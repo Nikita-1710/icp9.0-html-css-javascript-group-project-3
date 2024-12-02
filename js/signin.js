1bac3f4eeef165c712839e7ed6ee521cef09b7a9
yashyeole09 <yashyeole2121@gmail.com>
2024-12-02 18:52:52 +0530

    css

---
+++ js/signin.js
@@ -0,0 +1,11 @@
+function openModal(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display = 'flex';
+    overlayElement.style.buttoncolor = 'white';
+}
+
+function closeModal(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display = 'none';
+}
+
