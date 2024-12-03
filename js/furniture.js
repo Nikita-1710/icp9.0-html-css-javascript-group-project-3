ba916c86fa17d1da40563a05b2a6c63b6354161c
aman85662 <118710449+aman85662@users.noreply.github.com>
2024-12-04 05:25:14 +0530

    Smart-Thermostat-product added

--- js/furniture.js
+++ js/furniture.js
@@ -6,6 +6,7 @@ function closesidebar(){
     const sidebarElement = document.getElementById('sidebar');
     sidebarElement.style.visibility= 'hidden';
 }
+/*  slide-show*/
 const images = ["../img/electric-blanket-1.jpg","../img/electric-blanket-2.jpg","../img/electric-blanket-3.jpg","../img/electric-blanket-4.jpg"];
 
 let currentIndex = 0;
@@ -22,15 +23,27 @@ function prevImage() {
     }
     showImage(currentIndex);
 }
-function nextImage() {
-    if (currentIndex === images.length - 1) {
-        currentIndex = 0;
-    } else {
-        currentIndex++;
-    }
-    showImage(currentIndex);
-}
 setInterval(prevImage,3000);
+/**
+ smart-Thermostat 
+ */
+ const img = ["../img/Smart-thermostat-1.jpg","../img/Smart-thermostat-2.jpg","../img/Smart-thermostat-3.jpg","../img/Smart-thermostat-4.jpg","../img/Smart-thermostat-5.jpg"];
+
+ let current = 0;
+ 
+ function showI(index) {
+     const slideshowImage = document.getElementById('slideshow-img');
+     slideshowImage.src = img[index];
+ }
+ function prevI() {
+     if (current === 0) {
+         current = images.length - 1;
+     } else {
+         current--;
+     }
+     showI(current);
+ }
+ setInterval(prevI,3000);
 /* description menu */
 
 function descrip(){
